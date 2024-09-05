9cacbda0670dcd88b1ac06d3b355a6af59095552
Josue Costa <contato_josuecosta@hotmail.com>
2024-09-05 15:59:59 -0400

    Setup next project width typescript and styled components

---
+++ src/pages/_dcuments.tsx
@@ -0,0 +1,52 @@
+import Document, {Html, Head, NextScript, Main, DocumentContext, DocumentInitialProps} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document{
+    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage
+        
+        try{
+
+            ctx.renderPage = () => 
+                originalRenderPage({
+                    enhanceApp: (App)  => (props) =>
+                        sheet.collectStyles(<App {...props}/>) 
+            })
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return{
+                ...initialProps,
+                styles:(
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                )
+            }
+ 
+        } finally {
+            sheet.seal()
+        }
+    }
+
+    render() {
+      return (
+        <Html>
+            <Head>
+                <meta/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+                <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet"/>
+                
+                
+            </Head>
+            <body>
+                <Main/>
+                <NextScript/>
+            </body>
+        </Html>
+      )
+    }
+
+}
---
+++ src/styles/global.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        padding:0px;
+        margin:0px;
+        box-sizing:border-box;
+    }
+
+    html {
+        scroll-behavior:smoth;
+        font:500 16px 'Reboto Slab', sans;
+        color: #333;
+    }
+`
+
+
+
+export default GlobalStyle
